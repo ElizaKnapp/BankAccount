50684667bae5010b10e552ee04c6ab374e2ead6f
ElizaKnapp <eknapp20@stuy.edu>
2020-10-28 15:32:54 -0400

    Better test cases for deposit and withdraw. There are repeating decimals but that always happens in java if you don't contantly convert everything to int.

--- BankTester.java
+++ BankTester.java
@@ -1,11 +1,32 @@
 public class BankTester {
   public static void main(String[] args) {
+
+  //for testing the constructor
     BankAccount t1 = new BankAccount(3284, "snailbob");
     System.out.println(t1);
+    System.out.println(t1.getBalance()); //0
+    System.out.println(t1.getAccountID()); //3284
 
-    System.out.println(t1.getBalance());
-    System.out.println(t1.getAccountID());
+    System.out.println(t1.deposit(-8.34)); //false
+    System.out.println(t1.getBalance()); //0
 
-    t1.setPassword("843593"); //how do you get this to print the password
+    System.out.println(t1.deposit(8.34)); //true
+    System.out.println(t1.getBalance()); //8.34
+
+    System.out.println(t1.deposit(0)); //true
+    System.out.println(t1.getBalance()); //8.34
+
+    System.out.println(t1.withdraw(-19)); //false
+    System.out.println(t1.getBalance()); //8.34
+
+    System.out.println(t1.withdraw(19)); //false
+    System.out.println(t1.getBalance()); //8.34
+
+    System.out.println(t1.withdraw(8)); //true
+    System.out.println(t1.getBalance()); //0.34
+
+    System.out.println(t1.withdraw(0)); //true
+    System.out.println(t1.getBalance()); //0.34
+
   }
 }
